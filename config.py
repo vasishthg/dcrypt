78ea5203ebd01642492448bdda781b72fa5b313e
Vasishth Gupta <vasishthemail@gmail.com>
2021-12-26 22:32:37 +0530

    e

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from urllib import parse
 
-TOKEN = "OTI0Mjc2NDc2MTUxNDk2NzE0.YccNjA.sf9yVj0f4fi9EVMwnqoLEtkc2aY"
+TOKEN = "OTI0Mjc2NDc2MTUxNDk2NzE0.YccNjA.L1ilTaZPkfjXe8KERR9_zPDX3V4"
 CLIENT_SECRET= "lr3OG8zcNqobSxAcIZrNZHezKx3rCuFG"
 REDIRECT_URI = "http://127.0.0.1:5000/oauth/callback"
 OAUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id=924276476151496714&redirect_uri={parse.quote(REDIRECT_URI)}&response_type=code&scope=identify"
